23376968eb3a63ac001ccdc55a0abe1a9da402e3
Thomas Morris <tmorris@salsitasoft.com>
2019-12-19 23:02:34 +0100

    make factory for generating all stations

--- src/factories/station.ts
+++ src/factories/station.ts
@@ -1,4 +1,6 @@
+import { apiAllStations } from '../fixtures/lines/line1';
 import Station, {IStation} from '../lib/simulator/Station';
+
 const deepMerge = require('deepmerge')
 
 const defaultProps: IStation = {
@@ -13,6 +15,11 @@ export function station(props: Partial<IStation> = defaultProps): Station {
   return new Station(deepMerge(defaultProps, props));
 }
 
+export function allStations(): Station[] {
+  return apiAllStations.map(station => new Station(station));
+}
+
 export default {
-  station
+  station,
+  allStations,
 }
